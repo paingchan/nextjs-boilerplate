26c3d5a074b68f3f87fa853a42c58aa7c871d97a
Paing Chan <aung.paing.chan32@gmail.com>
2025-01-02 23:23:06 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans bg-gray-100">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
-        <h1 className="text-3xl font-semibold text-blue-600">ချမ်းမြတ်'s Exchange</h1>
+        <h1 className="text-3xl font-semibold text-blue-600">ချမ်းမြတ်&apos;s Exchange</h1>
 
         <div className="flex gap-4">
           <button
